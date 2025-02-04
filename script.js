bead25f26f447ec71b7e0f43fecefd8eba775327
Samuel Muhoho <muhoho644@gmail.com>
2025-02-04 15:37:34 +0300

    footer

---
+++ script.js
@@ -0,0 +1,6 @@
+let about= document.getElementById("About")
+
+let div1=document.createElement('div')
+    div1.textContent= ("Contact us for further information")
+    div1.style.position
+about.appendChild(div1)
